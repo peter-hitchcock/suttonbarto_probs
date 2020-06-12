7c758c361d9dc545ef8769d250453ec9b2dd15ba
Pete Hitchcock <ptrhitchcock@gmail.com>
2020-06-12 09:46:19 -0700

    experimenting with adding an RL superclass with dyna as subclass that inherits from it

---
+++ Pete/py/Modules/rl.py
@@ -0,0 +1,9 @@
+class RL:
+        def __init__(self, world, agent, control, Q_vals, model):
+        self.world = dict(world)
+        self.agent = dict(agent)
+        self.control = dict(control)
+
+
+
+
