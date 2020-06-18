80bc15d17285c29ff08d958557497dbf9b81cbe1
Pete Hitchcock <ptrhitchcock@gmail.com>
2020-06-17 17:17:23 -0700

    added dyna and rl classes

--- Pete/py/ch_8.py
+++ Pete/py/ch_8.py
@@ -3,6 +3,7 @@ import importlib
 #from importlib import reload as ir
 
 class Grid:
+    '''Contains everything needed to set up a grid world for your agent'''
     def __init__(self, HEIGHT=6, WIDTH=9):
         self.HEIGHT = HEIGHT
         self.WIDTH = WIDTH
@@ -15,6 +16,25 @@ class Grid:
         return 'This is a {self.HEIGHT} by {self.WIDTH}' \
                 ' world.'.format(self=self)    
 
+class RL_agent(Grid):
+    '''Defines generic RL agent that will be inherited to create specific agents'''
+    def __init__(self):
+        self.ACTIONS = {
+            "up": [0, 1],
+            "down": [0, -1],
+            "left": [-1, 0],
+            "right": [1, 0],
+        } 
+        # Initialize empty array of q values  
+        HEIGHT, WIDTH, ACTIONS = (Grid().HEIGHT, Grid().WIDTH, len(self.ACTIONS))
+        self.q_values = np.zeros([ACTIONS, HEIGHT, WIDTH])
+        
+class Dyna_agent(RL_agent):
+    def __init__(self): 
+        HEIGHT, WIDTH, ACTIONS = (Grid().HEIGHT, Grid().WIDTH, len(RL_agent().ACTIONS))       
+        self.s_prime = np.zeros([ACTIONS, HEIGHT, WIDTH])
+        self.reward = np.zeros([ACTIONS, HEIGHT, WIDTH])
+
 if __name__ == "__main__":
     print(str(Grid()))
     HEIGHT, WIDTH, OCCL_COLS, OCCL_ROWS = \
@@ -23,5 +43,7 @@ if __name__ == "__main__":
     view_grid_tmp = flat_repr.reshape(Grid().HEIGHT, Grid().WIDTH)
     view_grid_tmp[OCCL_COLS, OCCL_ROWS] = np.nan
     print(view_grid_tmp)
+    
+    
 
 
